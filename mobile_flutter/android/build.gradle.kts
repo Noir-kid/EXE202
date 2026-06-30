44bc1b18261d11290357c97f04d46952d65fc991
VO THANH QUOC HUY <HuyVTQSE172014@fpt.edu.vn>
2026-07-01 01:51:01 +0700

    update app

--- mobile_flutter/android/build.gradle.kts
+++ mobile_flutter/android/build.gradle.kts
@@ -5,31 +5,13 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory = newBuildDir
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
-subprojects {
+    project.layout.buildDirectory = newSubprojectBuildDir
     project.evaluationDependsOn(":app")
-}
-
-subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-            android.compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-        }
-    }
 }
 
 tasks.register<Delete>("clean") {
